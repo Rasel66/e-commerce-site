721d99a8bdaa2bb31a7a7b99e951bd6ca2e0dc11
Md. Rasel Hossain <59763029+Rasel66@users.noreply.github.com>
2021-06-07 10:18:50 +0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ecommerce_site.Startup))]
+namespace Ecommerce_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
